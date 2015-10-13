7d3ad41227a423f11827003151ab28312667782e
Jorge Castellano <jorgecm14@gmail.com>
2015-10-13 14:38:48 -0430

    inclusion de bootstrap y un poquito de estilos a la pagina

--- numero_secreto.js
+++ numero_secreto.js
@@ -1,30 +1,36 @@
-//Numero maximo
-var max = 50;
-//numero minimo
-var min = 1;
+function numero_secreto(){
+    //Numero maximo
+    var max = 10;
+    //numero minimo
+    var min = 1;
 
-var numero_secreto = Math.random() * max;
-var numero_secreto = parseInt(numero_secreto);
-console.log(numero_secreto);
+    var numero_secreto = Math.random() * max;
+    var numero_secreto = parseInt(numero_secreto);
 
-var mensaje = "Ingresa un número para adivinar el número secreto";
+    if (numero_secreto == 0) {
+      numero_secreto = 1;
+    }
+    console.log(numero_secreto);
 
-while(true){
-  
-  var numero_del_usuario = prompt(mensaje);
-  numero_del_usuario = parseInt(numero_del_usuario);
-  
-  if(numero_del_usuario === numero_secreto){
-    alert("Ganasteeee!! Adivinaste el número secreto");
-    break;
-  }
-  else if(numero_del_usuario === 0){
-    break;
-  }
-  else if(numero_del_usuario > numero_secreto){
-    mensaje = "Lo sentimos, el número que ingresaste es mayor al número secreto";
-  }
-  else if(numero_del_usuario < numero_secreto){
-        mensaje = "Lo sentimos, el número que ingresaste es menor al número secreto";
-  }
+    var mensaje = "Ingresa un número para adivinar el número secreto";
+
+    while(true){
+      
+        var numero_del_usuario = prompt(mensaje);
+        numero_del_usuario = parseInt(numero_del_usuario);
+
+        if(numero_del_usuario === numero_secreto){
+            alert("Ganasteeee!! Adivinaste el número secreto");
+            break;
+        }
+        else if(numero_del_usuario === 0){
+            break;
+        }
+        else if(numero_del_usuario > numero_secreto){
+            mensaje = "El número que ingresaste es mayor al número secreto";
+        }
+        else if(numero_del_usuario < numero_secreto){
+            mensaje = "El número que ingresaste es menor al número secreto";
+        }
+    }
 }
