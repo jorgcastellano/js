7acd99909949f7b0fac0a948971837565d9aad4a
Jorge Castellano <jorgecm14@gmail.com>
2015-10-13 23:01:18 -0430

    motor completo de ppt

--- ppt.js
+++ ppt.js
@@ -1,11 +1,63 @@
 function ppt(){
-	var min = 1;
-	var max = 3;
 
-	var cpu = Math.round(Math.random() * ((max + 0.4) - (min - 0.5)) + (min - 0.5));
+	var cpu = 0;
+	var jugador = 0;
 
-	console.log(cpu);
+	var leyenda = "¿Piedra (1)?\n¿papel (2)?\n¿Tijera (3)?";
+	var mensaje = leyenda;
+	var numero;
 
-	
-	
+	while((cpu < 3) && (jugador < 3)){
+
+		numero = Math.round(Math.random() * (3.4 - 0.5) + 0.5);
+		console.log(numero);
+
+		var usuario = parseInt(prompt(mensaje));
+
+		if (numero == 1 && usuario == 1) {
+			mensaje  = "Empate con piedra cada uno\n"+leyenda;
+		}
+		else if (numero == 1 && usuario == 2) {
+			mensaje = "Gana usuario con papel contra piedra\n"+leyenda;
+			jugador++;
+		}
+		else if (numero == 1 && usuario == 3) {
+			mensaje = "Gana computadora con piedra contra tijera\n"+leyenda;
+			cpu++;
+		}
+		else if (numero == 2 && usuario == 1) {
+			mensaje = "Gana computadora con papel contra piedra\n"+leyenda;
+			cpu++;
+		}
+		else if (numero == 2 && usuario == 2) {
+			mensaje = "Empate con papel cada uno\n"+leyenda;
+		}
+		else if (numero == 2 && usuario == 3) {
+			mensaje = "Gana usuario con tijera contra papel\n"+leyenda;
+			jugador++;
+		}
+		else if (numero == 3 && usuario == 1) {
+			mensaje = "Gana usuario con piedra contra tijera\n"+leyenda;
+			jugador++;
+		}
+		else if (numero == 3 && usuario == 2) {
+			mensaje = "Gana computadora con tijera contra papel\n"+leyenda;
+			cpu++;
+		}
+		else if (cpu == 3 && usuario == 3) {
+			mensaje = "Empate con tijera cada uno\n"+leyenda;
+		}
+		else if (usuario == 0){
+			break;
+		}
+		else {
+			mensaje = "Dato inválido, intentelo de nuevo\n"+leyenda;
+		}
+		cpu 
+	}
+	if (cpu === 3) {
+		alert("Ganador computadora\nUsuario " + jugador + " - " + cpu + " computadora\n");
+	} else if (jugador === 3) {
+		alert("¡Felicidades has ganado!\nUsuario " + jugador + " - " + cpu + " computadora\n");
+	}
 }
