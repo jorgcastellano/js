41c1fb73426bf0600f072beeb4d2060808021fce
Jorge Castellano <jorgecm14@gmail.com>
2016-09-03 21:54:40 -0400

    Generador de numeros aleatorios en el tablero

--- js/minas.js
+++ js/minas.js
@@ -9,22 +9,32 @@ function explosion(){
 
 function winner(){
 	accion.textContent = "Felicidades, has ganado la partida!";
+	comenzar.textContent = "Intentarlo de nuevo"
 }
 
 function loser(){
 	accion.textContent = "Lamentablemente has perdido, intenta de nuevo";
+	comenzar.textContent = "Intentarlo de nuevo"
 }
+function generador(){
+	var arreglo = [];
+	for (var i = 0; i < 5; i++) {
+		arreglo[i] = [];
+		for (var j = 0; j < 5; j++) {
+			arreglo[i][j] = Math.round(Math.random());
+			console.log(arreglo[i][j]);
+		}
+	}
+	return arreglo;
+}
+
 function campo_minado() {
 
 	//botonMinas.textContent = ""
 
 	//0 Cesped, 1 Bomba
-	var campo = [ 	[ 0, 1, 1, 0, 0],
-					[ 1, 0, 0, 1, 1],
-					[ 0, 1, 0, 0, 0],
-					[ 1, 0, 1, 0, 1],
-					[ 0, 1, 0, 0, 1]
-				];
+
+	var campo = generador();
 
 	var nombres = ["Cesped", "Bomba"];
 	var x, y, aciertos = 0, intentos = 0;
@@ -33,11 +43,12 @@ function campo_minado() {
 			"Ingrese la posición del campo a elegir (valores entre 0 y 4)");
 
 	while ((aciertos != 3) && (intentos < 5)) {
-		x = prompt ("Posicion X: ");
-		y = prompt ("Posicion Y: ");
+		x = prompt ("Posicion X: ", 0);
+		y = prompt ("Posicion Y: ", 0);
 
 		if ((x >= 0 && x <= 4)&&(y >= 0 && y <= 4)) {
 			var posicion = campo[x][y];
+			console.log(posicion);
 			alert ("Elegiste " + nombres[posicion]);
 			if (nombres[posicion] == "Cesped") {
 				acierto();
